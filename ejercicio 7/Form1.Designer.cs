89b4286e895d720bcfd49e98025f8aad01190681
tania <20250161@ricaldone.edu.sv>
2025-06-13 12:01:28 -0600

    ejercicio 7

---
+++ ejercicio 7/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ejercicio_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtConversion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tlpAyuda = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SeaGreen;
+            this.panel1.Controls.Add(this.btnFinalizar);
+            this.panel1.Controls.Add(this.btnLimpiar);
+            this.panel1.Controls.Add(this.btnConvertir);
+            this.panel1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.panel1.ForeColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(580, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(122, 258);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnFinalizar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnFinalizar.Image = global::ejercicio_7.Properties.Resources.icons8_cerrar_ventana_32;
+            this.btnFinalizar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFinalizar.Location = new System.Drawing.Point(0, 172);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(122, 83);
+            this.btnFinalizar.TabIndex = 2;
+            this.btnFinalizar.Text = "Finalizar";
+            this.btnFinalizar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnFinalizar.UseVisualStyleBackColor = false;
+            this.btnFinalizar.Click += new System.EventHandler(this.btnFinalizar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnLimpiar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLimpiar.Image = global::ejercicio_7.Properties.Resources.icons8_limpiar_32;
+            this.btnLimpiar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLimpiar.Location = new System.Drawing.Point(0, 86);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(122, 86);
+            this.btnLimpiar.TabIndex = 0;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnConvertir.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnConvertir.Image = global::ejercicio_7.Properties.Resources.icons8_convertir_32;
+            this.btnConvertir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConvertir.Location = new System.Drawing.Point(0, 0);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(122, 86);
+            this.btnConvertir.TabIndex = 1;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnConvertir.UseVisualStyleBackColor = false;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.SeaGreen;
+            this.groupBox3.Controls.Add(this.txtCantidad);
+            this.groupBox3.Controls.Add(this.txtConversion);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.pictureBox2);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.groupBox3.ForeColor = System.Drawing.Color.SeaShell;
+            this.groupBox3.Location = new System.Drawing.Point(27, 39);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(494, 307);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Comversión dolares a pesos";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(154, 80);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(210, 27);
+            this.txtCantidad.TabIndex = 11;
+            this.tlpAyuda.SetToolTip(this.txtCantidad, "Ingresa una cantidad de dinero en dolares");
+            // 
+            // txtConversion
+            // 
+            this.txtConversion.Location = new System.Drawing.Point(218, 197);
+            this.txtConversion.Name = "txtConversion";
+            this.txtConversion.ReadOnly = true;
+            this.txtConversion.Size = new System.Drawing.Size(227, 27);
+            this.txtConversion.TabIndex = 10;
+            this.tlpAyuda.SetToolTip(this.txtConversion, "Convierte la cantidad de dolares a pesos");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(240, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Conversion a pesos:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ejercicio_7.Properties.Resources.icons8_dinero_64;
+            this.pictureBox2.Location = new System.Drawing.Point(43, 143);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(191, 118);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(137, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(251, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ingresa la cantidad de dolares:";
+            // 
+            // tlpAyuda
+            // 
+            this.tlpAyuda.ToolTipTitle = "Ayuda";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 370);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Conversion de dinero";
+            this.panel1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnFinalizar;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtConversion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolTip tlpAyuda;
+    }
+}
+
---
+++ ejercicio 7/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double dinero, conersion;
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtConversion.Clear();
+            txtCantidad.Clear();
+            txtCantidad.ReadOnly = false;
+        }
+
+        private void btnFinalizar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtCantidad.Text, out dinero))
+            {
+
+                if (dinero >= 0)
+                {
+                    conersion = dinero * 18;
+                    txtConversion.Text = conersion.ToString("N2");
+                }
+
+                else
+                {
+                    txtConversion.Text = "Ingrese numeros positivos";
+                }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(txtCantidad.Text))
+                {
+                    txtConversion.Text = "Existe un campo vacio";
+                }
+
+                else
+                {
+                    txtConversion.Text = "Ingresa numeros y no letras";
+                }
+            }
+
+            txtCantidad.ReadOnly = true;
+        }
+    }
+}
